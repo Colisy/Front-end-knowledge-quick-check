0ca2a69696f75023b53f90ed377bb530a1d42447
fridaysunnyd@gmail.com <weijiadang>
2020-04-13 17:36:32 +0800

    jieliu

--- 手写代码/throttleAndDebounce.js
+++ 手写代码/throttleAndDebounce.js
@@ -1,6 +1,6 @@
 //节流用throttle包装函数
 //只在单位时间内执行一次,第一次执行
-//下拉加载
+//缩放
 function throttle(event, time) {
   let pre = 0;
   return function (...args) {
@@ -12,7 +12,7 @@ function throttle(event, time) {
 }
 //防抖用debounce包装函数
 //事件触发n秒后才执行
-//联想搜索 窗口大小变化 表单验证
+//联想搜索 表单验证
 function debounce(event, time) {
   let timer = null;
   return function (...args) {
